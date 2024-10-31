552e75ec7fd64f901176c7a07c3486d926b7722e
Joel Victor <jnull.rx@gmail.com>
2024-10-31 13:20:25 -0300

    Adicionando Document no UserDTO

--- src/PrismaPrimeInvest.Application/DTOs/UserDTOs/UserDto.cs
+++ src/PrismaPrimeInvest.Application/DTOs/UserDTOs/UserDto.cs
@@ -3,6 +3,7 @@ public class UserDto : BaseDto
 {
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
+    public string? Document { get; set; }
     public required DateTime CreatedAt { get; set; }
     public required DateTime UpdatedAt { get; set; }
 }
